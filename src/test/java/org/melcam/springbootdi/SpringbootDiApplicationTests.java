a289f3384feec33938e496a10b7911274f9bacfa
melcam <yumelcam.93@gmail.com>
2024-02-26 23:04:38 +0100

    commit inicial

---
+++ src/test/java/org/melcam/springbootdi/SpringbootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.melcam.springbootdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
